de6e938cfd0ede277eeded61d4d007bc644352a9
Nemanja <nemanjafilipovic032@gmail.com>
2019-11-10 14:35:20 +0100

    Enter modals boiiii

--- app.js
+++ app.js
@@ -19,3 +19,33 @@ buttons.addEventListener('click', function (e) {
     }
 });
 
+// popup exit
+
+const popup = document.querySelector('.popup');
+window.addEventListener('click', clickOutside);
+
+function clickOutside(e) {
+    if (e.target == popup) {
+        popup.style.display = 'none';
+
+    } else {
+        popup.style.display = 'block';
+
+    }
+}
+
+//
+
+
+const popup1 = document.querySelector('#popup2');
+window.addEventListener('click', clickOutsidee);
+
+function clickOutsidee(e) {
+    if (e.target == popup1) {
+        popup1.style.display = 'none';
+
+    } else {
+        popup1.style.display = 'block';
+
+    }
+}
